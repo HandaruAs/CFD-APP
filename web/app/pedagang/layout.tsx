123ba4aebb4f84ac81a926ca2c363e1daa031b40
Handaru <handaruarioseto12@gmail.com>
2026-09-23 09:57:27 +0700

    perpindahan menu ke superadmin

--- web/app/pedagang/layout.tsx
+++ web/app/pedagang/layout.tsx
@@ -1,5 +1,3 @@
-import "./pedagang.css";
-
 export default function PedagangLayout({ children }: { children: React.ReactNode }) {
   return (
     <div data-pedagang-scope className="flex flex-col h-full min-h-screen">
